5674a87f2b988d80d919cbe2dd2b0e60261efe4d
vpvpvpvp <vktrpnzrv@gmail.com>
2021-10-04 21:27:06 +0300

    add cors

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 from fastapi import FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.db import init_db, SessionLocal
 from app.api.v1 import api_router
@@ -13,5 +14,15 @@ db = SessionLocal()
 init_db(db)
 
 app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(api_router)
 app.mount("/upload", StaticFiles(directory=settings.STATIC_FILES), name="upload")
