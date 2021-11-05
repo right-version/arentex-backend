c6970d553667185887b00af3fc2ce0b08805fac4
vpvpvpvp <vktrpnzrv@gmail.com>
2021-11-05 22:21:05 +0300

    categories sorted by count

--- app/api/v1/category.py
+++ app/api/v1/category.py
@@ -1,9 +1,10 @@
 from typing import List
 
 from fastapi import APIRouter, Depends, HTTPException, Path
+from sqlalchemy import func, text
 from sqlalchemy.orm import Session
 
-from app.models import Category
+from app.models import Category, Product
 from app.schemas import CategoryInDB
 from app.db.base import get_db
 
@@ -11,7 +12,20 @@ from app.db.base import get_db
 router = APIRouter()
 
 
+# @router.get("/", response_model=List[CategoryInDB])
+# def get_categories(db: Session = Depends(get_db)):
+#     categories = db.query(Category).all()
+#     return categories
+
+
 @router.get("/", response_model=List[CategoryInDB])
-def get_categories(db: Session = Depends(get_db)):
-    categories = db.query(Category).all()
+def get_top_categories(db: Session = Depends(get_db)):
+    categories = (
+        db.query(Category, func.count(Product.category_id).label("total"))
+        .outerjoin(Product)
+        .group_by(Category)
+        .order_by(text("total DESC"))
+        .all()
+    )
+    categories = [c[0] for c in categories]
     return categories
